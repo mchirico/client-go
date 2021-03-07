b71d7b616da8ccdc445151983dadf122d5b96696
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2021-03-07 06:50:09 -0500

    create CRD

--- go.mod
+++ go.mod
@@ -3,10 +3,25 @@ module github.com/mchirico/client-go
 go 1.15
 
 require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/googleapis/gnostic v0.5.4 // indirect
 	github.com/imdario/mergo v0.3.11 // indirect
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
-	k8s.io/apimachinery v0.19.0-rc.4
-	k8s.io/client-go v0.19.0-rc.4
-	k8s.io/utils v0.0.0-20200821003339-5e75c0163111 // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93 // indirect
+	golang.org/x/sys v0.0.0-20210305230114-8fe3ee5dd75b // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	k8s.io/api v0.20.4
+	k8s.io/apimachinery v0.20.4
+	k8s.io/client-go v0.20.4
+	k8s.io/klog/v2 v2.6.0 // indirect
+	k8s.io/utils v0.0.0-20210305010621-2afb4311ab10 // indirect
+	sigs.k8s.io/structured-merge-diff/v3 v3.0.1-0.20200706213357-43c19bbb7fba // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	v1 "k8s.io/api/core/v1"
-	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"log"
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-func PodWatch() {
+func WatchExperiment() {
 	const shortDuration = 60 * time.Minute
 
 	d := time.Now().Add(shortDuration)
@@ -44,44 +44,44 @@ func PodWatch() {
 	namespace := "default"
 	name := "name3"
 	event := &v1.Event{
-		TypeMeta:            metaV1.TypeMeta{},
-		ObjectMeta:          metaV1.ObjectMeta{Namespace: namespace,Name: name},
+		TypeMeta:            metav1.TypeMeta{},
+		ObjectMeta:          metav1.ObjectMeta{Namespace: namespace,Name: name},
 		InvolvedObject:      v1.ObjectReference{},
 		Reason:              "Some silly reason",
 		Message:             "Message... some random message here... ",
 		Source:              v1.EventSource{},
-		FirstTimestamp:      metaV1.Time{time.Now()},
-		LastTimestamp:       metaV1.Time{ time.Now().Add(-78 * time.Minute)},
+		FirstTimestamp:      metav1.Time{time.Now()},
+		LastTimestamp:       metav1.Time{time.Now().Add(-78 * time.Minute)},
 		Count:               10,
 		Type:                "Special",
-		EventTime:           metaV1.MicroTime{},
+		EventTime:           metav1.MicroTime{},
 		Series:              nil,
 		Action:              "Dropped jaw",
 		Related:             nil,
 		ReportingController: "A Giant Panda",
 		ReportingInstance:   "",
 	}
-	_ , err = coreV1Client.Events(namespace).Create(ctx, event , metaV1.CreateOptions{})
+	_ , err = coreV1Client.Events(namespace).Create(ctx, event , metav1.CreateOptions{})
 
 	event.Action = "Smile"
-	coreV1Client.Events(namespace).Update(ctx,event,metaV1.UpdateOptions{})
+	coreV1Client.Events(namespace).Update(ctx,event, metav1.UpdateOptions{})
 
 
 
 	if err != nil {
-		coreV1Client.Events(namespace).Delete(ctx, name , metaV1.DeleteOptions{})
+		coreV1Client.Events(namespace).Delete(ctx, name , metav1.DeleteOptions{})
 		log.Fatal(err.Error())
 	}
 
 	go func() {
 		time.Sleep(7 * time.Second)
-		err = coreV1Client.Events(namespace).Delete(ctx, name , metaV1.DeleteOptions{})
+		err = coreV1Client.Events(namespace).Delete(ctx, name , metav1.DeleteOptions{})
 	}()
 
 
-	watch, err := coreV1Client.Events("").Watch(ctx, metaV1.ListOptions{})
+	watch, err := coreV1Client.Events("").Watch(ctx, metav1.ListOptions{})
 
-	//watch, err := coreV1Client.Pods("").Watch(ctx, metaV1.ListOptions{})
+	//watch, err := coreV1Client.Pods("").Watch(ctx, metav1.ListOptions{})
 
 	if err != nil {
 		log.Fatal(err.Error())
@@ -126,7 +126,7 @@ func main() {
 	// Get a *PodList (k8s.io/api/core/v1)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	pods, err := coreV1Client.Pods("").List(ctx, metaV1.ListOptions{})
+	pods, err := coreV1Client.Pods("").List(ctx, metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
@@ -141,7 +141,7 @@ func main() {
 	//-------------------------------------------------------------------------//
 
 	// Get a *NodeList (k8s.io/api/core/v1)
-	nodes, err := coreV1Client.Nodes().List(ctx, metaV1.ListOptions{})
+	nodes, err := coreV1Client.Nodes().List(ctx, metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
@@ -156,7 +156,7 @@ func main() {
 	//-------------------------------------------------------------------------//
 
 	// Get a *DeploymentList (k8s.io/api/apps/v1)
-	deployments, err := appsV1Client.Deployments("").List(ctx, metaV1.ListOptions{})
+	deployments, err := appsV1Client.Deployments("").List(ctx, metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
@@ -166,10 +166,42 @@ func main() {
 		fmt.Printf("Deployment %d: %s\n", i+1, deployment.ObjectMeta.Name)
 	}
 
+
+	//-------------------------------------------------------------------------//
+	// Create Namespace
+	//-------------------------------------------------------------------------//
+
+
+	nsSpec := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-bubble"}}
+	_, err = clientset.CoreV1().Namespaces().Create(context.TODO(),nsSpec, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Errorf("err:%w",err)
+	}
+
+
+	//-------------------------------------------------------------------------//
+	// CRD
+	//-------------------------------------------------------------------------//
+
+
+	//nsSpec := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-bubble"}}
+	//_, err = clientset.CoreV1().Namespaces().Create(context.TODO(),nsSpec, metav1.CreateOptions{})
+	//if err != nil {
+	//	fmt.Errorf("err:%w",err)
+	//}
+
+
+
+
+
+
+
+
+
 	fmt.Printf("\n\nGetting Ready to run watch:\nCtl-c to kill\n\n")
 	time.Sleep(2 * time.Second)
 	fmt.Printf("")
 
-	PodWatch()
+	WatchExperiment()
 
 }
